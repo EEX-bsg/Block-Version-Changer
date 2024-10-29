da1a1b6b9c51d722f6c0e97546150ce2f5cac8fb
EEX-bsg <exendra314@gmail.com>
2024-10-30 00:11:58 +0900

    init commit

---
+++ src/BlockVersionChanger/Mod.cs
@@ -0,0 +1,14 @@
+using System;
+using Modding;
+using UnityEngine;
+
+namespace BlockVersionChanger
+{
+	public class Mod : ModEntryPoint
+	{
+		public override void OnLoad()
+		{
+			// Called when the mod is loaded.
+		}
+	}
+}
